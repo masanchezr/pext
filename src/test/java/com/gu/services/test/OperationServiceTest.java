cdd58b54c268786a5cf32628acaaeea4f0fc5c3d
MariaAngeles <MariAngeles@Mariangeles>
2018-07-01 22:59:16 +0200

    Cambio error en jsp al actualizar operación

---
+++ src/test/java/com/gu/services/test/OperationServiceTest.java
@@ -0,0 +1,28 @@
+package com.gu.services.test;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gu.dbaccess.entities.OperationEntity;
+import com.gu.services.operations.OperationService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:application-context-test.xml" })
+public class OperationServiceTest {
+
+	@Autowired
+	private OperationService operationservice;
+
+	@Test
+	public void updateOperationTest() {
+		OperationEntity operation = operationservice.findById(17L);
+		operation.setAmount(new BigDecimal(100));
+		operationservice.update(operation);
+	}
+
+}
