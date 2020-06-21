968039b535cfba056e5e987df8b6599f18d2b416
MariaAngeles <MariAngeles@Mariangeles>
2020-06-21 17:16:25 +0200

    Horas extra

--- src/main/java/com/gu/employee/controllers/ExtraHoursController.java
+++ src/main/java/com/gu/employee/controllers/ExtraHoursController.java
@@ -1,5 +1,7 @@
 package com.gu.employee.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gu.dbaccess.entities.EmployeeEntity;
 import com.gu.employee.forms.ExtraHours;
+import com.gu.services.employees.EmployeeService;
 import com.gu.services.extrahours.ExtraHourService;
 
 @Controller
 public class ExtraHoursController {
 
+	@Autowired
+	private EmployeeService employeeservice;
+
+	@Autowired
 	private ExtraHourService extraHourService;
 
 	@GetMapping("/employee/extrahours")
@@ -29,6 +37,9 @@ public class ExtraHoursController {
 			model.setViewName("/employee/extrahours");
 			model.addObject("extrahours", extrahours);
 		} else {
+			String user = SecurityContextHolder.getContext().getAuthentication().getName();
+			EmployeeEntity employee = employeeservice.getEmployeeByUserName(user);
+			extrahours.setEmployee(employee);
 			extraHourService.save(extrahours);
 			model.setViewName("/employee/success");
 		}
