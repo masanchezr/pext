a95e740c68c0ecc5de5f109d9f6c789f7d43b6a5
masanchezr <webmariangeless@hotmail.com>
2022-05-18 20:40:59 +0200

    No sé que estoy subiendo

---
+++ src/main/java/com/sboot/golden/converters/TPVConverter.java
@@ -0,0 +1,24 @@
+package com.sboot.golden.converters;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sboot.golden.boss.forms.TPV;
+import com.sboot.golden.dbaccess.entities.TPVEntity;
+import com.sboot.golden.util.date.DateUtil;
+
+@Component
+public class TPVConverter {
+
+	/** The mapper. */
+	@Autowired
+	private ModelMapper mapper;
+
+	public TPVEntity convertToEntity(TPV tpv) {
+		TPVEntity entity = mapper.map(tpv, TPVEntity.class);
+		entity.setCreationdate(DateUtil.getDate(tpv.getSdate()));
+		return entity;
+	}
+
+}
