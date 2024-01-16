54dfdc167ba29af541d05636371cbff1d610726d
masanchezr <webmariangeless@hotmail.com>
2024-01-16 16:29:23 +0100

    Update GoldenErrorController.java
    No funcionaba el 404

--- src/main/java/com/atmj/GoldenErrorController.java
+++ src/main/java/com/atmj/GoldenErrorController.java
@@ -3,13 +3,17 @@ package com.atmj;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 public class GoldenErrorController implements ErrorController {
+
 	@RequestMapping("/error")
 	@ResponseBody
-	String error(HttpServletRequest request) {
-		return "/error";
+	ModelAndView error(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("404");
+		return model;
 	}
 }
