3de668af1248e9b9717e26299fbb49c83ee6fa2d
masanchezr <webmariangeless@hotmail.com>
2024-02-01 10:54:24 +0100

    Update ChangeMachineServiceTest.java
    Añado un test

--- src/test/java/com/atmj/gsboot/test/ChangeMachineServiceTest.java
+++ src/test/java/com/atmj/gsboot/test/ChangeMachineServiceTest.java
@@ -1,5 +1,8 @@
 package com.atmj.gsboot.test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,16 @@ class ChangeMachineServiceTest {
 	@Test
 	void getIncomeTotalMonthTest() {
 		Assertions.assertNull(changeMachineService.getIncomeTotalMonth());
+	}
+
+	@Test
+	void rechargesTest() {
+		Calendar c = Calendar.getInstance();
+		Date now = c.getTime();
+		c.set(Calendar.YEAR, 2022);
+		c.set(Calendar.MONTH, Calendar.DECEMBER);
+		c.set(Calendar.DAY_OF_MONTH, 30);
+		Assertions.assertNotNull(changeMachineService.recharges(c.getTime(), now));
 	}
 
 	@Test
